4a2a96b2067c8fd452254fd2b9033d4e238375cb
matikant <fe-2003@hotmail.com>
2024-07-23 01:48:33 +0700

    fix: edit schema

--- backend/models/Form.js
+++ backend/models/Form.js
@@ -13,273 +13,240 @@ const FormSchema = new mongoose.Schema(
     },
 
     // 1-4
-    generalInformation: {
-      age: {
-        type: Number,
-      },
-      sex: {
-        type: Number,
-      },
-      mentalDisorder: {
-        type: Number,
-      },
-      occupation: {
-        type: String,
-      },
+    age: {
+      type: Number,
+    },
+    sex: {
+      type: Number,
+    },
+    mentalDisorder: {
+      type: Number,
+    },
+    occupation: {
+      type: String,
     },
 
     // 1-4_1
-    educationInformation: {
-      gpax: {
-        type: Number,
-      },
-      gpaxSatisfaction: {
-        type: Number,
-      },
-      university: {
-        type: Number,
-      },
-      year: {
-        type: Number,
-      },
+    gpax: {
+      type: Number,
+    },
+    gpaxSatisfaction: {
+      type: Number,
+    },
+    university: {
+      type: Number,
+    },
+    year: {
+      type: Number,
     },
 
     // 2-1 to 2-9
-    cbiss: {
-      personalBurnout: {
-        cbissQ1: {
-          type: Number,
-        },
-        cbissQ2: {
-          type: Number,
-        },
-        cbissQ3: {
-          type: Number,
-        },
-        cbissQ4: {
-          type: Number,
-        },
-        cbissQ5: {
-          type: Number,
-        },
-        cbissQ6: {
-          type: Number,
-        },
-      },
-
-      studyRelatedBurnout1: {
-        cbissQ7: {
-          type: Number,
-        },
-        cbissQ8: {
-          type: Number,
-        },
-        cbissQ9: {
-          type: Number,
-        },
-        cbissQ10: {
-          type: Number,
-        },
-        cbissQ11: {
-          type: Number,
-        },
-        cbissQ12: {
-          type: Number,
-        },
-        cbissQ13: {
-          type: Number,
-        },
-      },
-
-      colleagueRelatedBurnout: {
-        cbissQ14: {
-          type: Number,
-        },
-        cbissQ15: {
-          type: Number,
-        },
-        cbissQ16: {
-          type: Number,
-        },
-        cbissQ17: {
-          type: Number,
-        },
-        cbissQ18: {
-          type: Number,
-        },
-        cbissQ19: {
-          type: Number,
-        },
-      },
-
-      teacherRelatedBurnout: {
-        cbissQ20: {
-          type: Number,
-        },
-        cbissQ21: {
-          type: Number,
-        },
-        cbissQ22: {
-          type: Number,
-        },
-        cbissQ23: {
-          type: Number,
-        },
-        cbissQ24: {
-          type: Number,
-        },
-        cbissQ25: {
-          type: Number,
-        },
-      },
+    cbissQ1: {
+      type: Number,
+    },
+    cbissQ2: {
+      type: Number,
+    },
+    cbissQ3: {
+      type: Number,
+    },
+    cbissQ4: {
+      type: Number,
+    },
+    cbissQ5: {
+      type: Number,
+    },
+    cbissQ6: {
+      type: Number,
+    },
+    cbissQ7: {
+      type: Number,
+    },
+    cbissQ8: {
+      type: Number,
+    },
+    cbissQ9: {
+      type: Number,
+    },
+    cbissQ10: {
+      type: Number,
+    },
+    cbissQ11: {
+      type: Number,
+    },
+    cbissQ12: {
+      type: Number,
+    },
+    cbissQ13: {
+      type: Number,
+    },
+    cbissQ14: {
+      type: Number,
+    },
+    cbissQ15: {
+      type: Number,
+    },
+    cbissQ16: {
+      type: Number,
+    },
+    cbissQ17: {
+      type: Number,
+    },
+    cbissQ18: {
+      type: Number,
+    },
+    cbissQ19: {
+      type: Number,
+    },
+    cbissQ20: {
+      type: Number,
+    },
+    cbissQ21: {
+      type: Number,
+    },
+    cbissQ22: {
+      type: Number,
+    },
+    cbissQ23: {
+      type: Number,
+    },
+    cbissQ24: {
+      type: Number,
+    },
+    cbissQ25: {
+      type: Number,
     },
 
     // canva #33-41
-    ri9: {
-      ri9Q1: {
-        type: Number,
-      },
-      ri9Q2: {
-        type: Number,
-      },
-      ri9Q3: {
-        type: Number,
-      },
-      ri9Q4: {
-        type: Number,
-      },
-      ri9Q5: {
-        type: Number,
-      },
-      ri9Q6: {
-        type: Number,
-      },
-      ri9Q7: {
-        type: Number,
-      },
-      ri9Q8: {
-        type: Number,
-      },
-      ri9Q9: {
-        type: Number,
-      },
+    ri9Q1: {
+      type: Number,
+    },
+    ri9Q2: {
+      type: Number,
+    },
+    ri9Q3: {
+      type: Number,
+    },
+    ri9Q4: {
+      type: Number,
+    },
+    ri9Q5: {
+      type: Number,
+    },
+    ri9Q6: {
+      type: Number,
+    },
+    ri9Q7: {
+      type: Number,
+    },
+    ri9Q8: {
+      type: Number,
+    },
+    ri9Q9: {
+      type: Number,
     },
 
     // canva #48-51
-    dass21: {
-      dass21Q1: {
-        type: Number,
-      },
-      dass21Q2: {
-        type: Number,
-      },
-      dass21Q3: {
-        type: Number,
-      },
-      dass21Q4: {
-        type: Number,
-      },
-      dass21Q5: {
-        type: Number,
-      },
-      dass21Q6: {
-        type: Number,
-      },
-      dass21Q7: {
-        type: Number,
-      },
-      dass21Q8: {
-        type: Number,
-      },
-      dass21Q9: {
-        type: Number,
-      },
-      dass21Q10: {
-        type: Number,
-      },
-      dass21Q11: {
-        type: Number,
-      },
-      dass21Q12: {
-        type: Number,
-      },
-      dass21Q13: {
-        type: Number,
-      },
-      dass21Q14: {
-        type: Number,
-      },
-      dass21Q15: {
-        type: Number,
-      },
-      dass21Q16: {
-        type: Number,
-      },
-      dass21Q17: {
-        type: Number,
-      },
-      dass21Q18: {
-        type: Number,
-      },
-      dass21Q19: {
-        type: Number,
-      },
-      dass21Q20: {
-        type: Number,
-      },
-      dass21Q21: {
-        type: Number,
-      },
+    dass21Q1: {
+      type: Number,
+    },
+    dass21Q2: {
+      type: Number,
+    },
+    dass21Q3: {
+      type: Number,
+    },
+    dass21Q4: {
+      type: Number,
+    },
+    dass21Q5: {
+      type: Number,
+    },
+    dass21Q6: {
+      type: Number,
+    },
+    dass21Q7: {
+      type: Number,
+    },
+    dass21Q8: {
+      type: Number,
+    },
+    dass21Q9: {
+      type: Number,
+    },
+    dass21Q10: {
+      type: Number,
+    },
+    dass21Q11: {
+      type: Number,
+    },
+    dass21Q12: {
+      type: Number,
+    },
+    dass21Q13: {
+      type: Number,
+    },
+    dass21Q14: {
+      type: Number,
+    },
+    dass21Q15: {
+      type: Number,
+    },
+    dass21Q16: {
+      type: Number,
+    },
+    dass21Q17: {
+      type: Number,
+    },
+    dass21Q18: {
+      type: Number,
+    },
+    dass21Q19: {
+      type: Number,
+    },
+    dass21Q20: {
+      type: Number,
+    },
+    dass21Q21: {
+      type: Number,
     },
 
     // 2-11 to 2-13
-    dreem12: {
-      learningFormat: {
-        dreem12Q1: {
-          type: Number,
-        },
-        dreem12Q2: {
-          type: Number,
-        },
-      },
-      teacher: {
-        dreem12Q3: {
-          type: Number,
-        },
-        dreem12Q4: {
-          type: Number,
-        },
-        dreem12Q5: {
-          type: Number,
-        },
-      },
-      academic: {
-        dreem12Q6: {
-          type: Number,
-        },
-        dreem12Q7: {
-          type: Number,
-        },
-        dreem12Q8: {
-          type: Number,
-        },
-      },
-      atmosphere: {
-        dreem12Q9: {
-          type: Number,
-        },
-        dreem12Q10: {
-          type: Number,
-        },
-      },
-      social: {
-        dreem12Q11: {
-          type: Number,
-        },
-        dreem12Q12: {
-          type: Number,
-        },
-      },
+    dreem12Q1: {
+      type: Number,
+    },
+    dreem12Q2: {
+      type: Number,
+    },
+    dreem12Q3: {
+      type: Number,
+    },
+    dreem12Q4: {
+      type: Number,
+    },
+    dreem12Q5: {
+      type: Number,
+    },
+    dreem12Q6: {
+      type: Number,
+    },
+    dreem12Q7: {
+      type: Number,
+    },
+    dreem12Q8: {
+      type: Number,
+    },
+    dreem12Q9: {
+      type: Number,
+    },
+    dreem12Q10: {
+      type: Number,
+    },
+    dreem12Q11: {
+      type: Number,
+    },
+    dreem12Q12: {
+      type: Number,
     },
   },
   {
